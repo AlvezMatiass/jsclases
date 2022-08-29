ff53b061733b5286b2f10dee8cf9aa2e40df7da9
Matias Alvez <matias2018alvez@gmail.com>
2022-08-29 17:59:30 -0300

    Version 4

--- clase8/parte3/main.js
+++ clase8/parte3/main.js
@@ -1,11 +1,21 @@
 
 
 function agregar(){
+
     let producto = document.getElementById("producto");
     let lista = document.getElementById("lista");
+    let precio = document.getElementById("precio");
 
-    let texto = document.createElement("li");
-    texto.innerText = producto.value;
+    let texto = document.createElement("p");
+    texto.innerText = "Producto: " + producto.value;
     lista.append(texto);
+
+    let texto_precio = document.createElement("p");
+    texto_precio.innerText = "Precio: " + precio.value;
+    lista.append(texto_precio);
+
+    let separar = document.createElement("p");
+    separar.innerText = "-----------------------------------------------";
+    lista.append(separar);
 
 }
